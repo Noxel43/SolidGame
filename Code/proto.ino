89ccd28e27d4e37f84eb856972da5b7d2cdfd623
Noxel43 <grtisepic@gmail.com>
2026-06-08 12:45:23 +1000

    did stuff

--- Code/proto.ino
+++ Code/proto.ino
@@ -6,4 +6,4 @@ port = "COM7"
 ser = serial.Serial(port, 9600, timeout=1)
 time.sleep(2)
 
-ser.write(b"Hello")
+ser.write(b"Hello\n")
--- Code/proto.py
+++ Code/proto.py
@@ -10,5 +10,5 @@ time.sleep(2)
 
 while True:
     if ser.in_waiting > 0:
-        input = ser.readline().decode('utf-8').strip()
-        print(input)
+        data = ser.readline().decode('utf-8').strip()
+        print(data)
